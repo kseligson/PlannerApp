ab3fdb37cc39d90ccd0fad613a7591127691b0a4
Kaitlin Seligson <kseligso@ucsd.edu>
2016-02-05 18:15:39 +0000

    Fixed small heroku issue

--- routes/settings.js
+++ routes/settings.js
@@ -1,9 +1,6 @@
 exports.view = function(req, res) {
 	//controller code goes here
 	var name = req.params.name; 
-	res.render('settings', {
-		'projectName': name
-	});
 
 	res.render( 'settings', {
 	'list-item': [
