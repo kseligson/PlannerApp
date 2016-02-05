dfdb986e67cfaf4b4fad4f4a930dc0f4dd913b6e
vagrant <vagrant@precise32.(none)>
2016-02-05 16:44:37 +0000

    Formatted initial FullCalendar

--- public/js/cal.js
+++ public/js/cal.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 	//page ready, init calendar
 	$('#calendar').fullCalendar({
 		//put options and callbacks here
-		
+		header: {
+			left: 'today prev,next',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		}
 	});
 });
